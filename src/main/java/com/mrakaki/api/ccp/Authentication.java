c4d720da18de91f75063b1a8549b20899d1f04e4
Alex Vargas <vb.alexr@gmail.com>
2025-09-29 23:30:42 +0000

    feat: added function to refresh token

--- src/main/java/com/mrakaki/api/ccp/Authentication.java
+++ src/main/java/com/mrakaki/api/ccp/Authentication.java
@@ -29,16 +29,8 @@ public class Authentication {
             if (codeInfo == null) {
                 return new Response<>(null, Optional.of(new com.mrakaki.api.dtos.Error(500, "Error during authentication")), 0, 1, "");
             }
-            String[] chunks = codeInfo.access_token().split("\\.");
 
-            Base64.Decoder decoder = Base64.getUrlDecoder();
-            String payload = new String(decoder.decode(chunks[1]));
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode node = objectMapper.readTree(payload);
-            int characterId = Integer.parseInt(node.get("sub").asText().split(":")[2]);
-            String characterName = node.get("name").asText();
-            return new Response<>(new AuthenticationInformation(characterId, characterName, codeInfo.access_token(), codeInfo.refresh_token()), Optional.empty(), 0, 1, "");
+            return new Response<>(authenticationInformationFromToken(codeInfo), Optional.empty(), 0, 1, "");
         } catch (Exception ex) {
             return new Response<>(null, Optional.of(new com.mrakaki.api.dtos.Error(500, ex.getMessage())), 0, 1, null);
         }
@@ -47,6 +39,40 @@ public class Authentication {
     public static String GetAuthenticationUrl(String redirectUrl, String authorizationUrl, String clientId, Set<String> characterScopes, String state) {
         return authorizationUrl + "?response_type=code&redirect_uri=" + encodeValue(redirectUrl) + "&client_id=" + clientId + "&scope=" + encodeValue(String.join(",", characterScopes)) + "&state=" + state;
     }
+
+    public static Response<AuthenticationInformation> RefreshToken(String refreshToken, String clientId, String clientSecret, String tokenUri) {
+        try {
+            var restClient = Common.GetRestClient("Authentication", Map.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE, HttpHeaders.AUTHORIZATION, basicAuthentication(clientId, clientSecret)));
+            var body = "grant_type=refresh_token&refresh_token=" + encodeValue(refreshToken);
+            var codeResponse = restClient.post().uri(tokenUri).body(body).retrieve().toEntity(AuthenticationResponse.class);
+            if (codeResponse.getStatusCode() != HttpStatus.OK) {
+                return new Response<>(null, Optional.of(new com.mrakaki.api.dtos.Error(codeResponse.getStatusCode().value(), "Error during authentication")), 0, 1, "");
+            }
+            var codeInfo = codeResponse.getBody();
+            if (codeInfo == null) {
+                return new Response<>(null, Optional.of(new com.mrakaki.api.dtos.Error(500, "Error during authentication")), 0, 1, "");
+            }
+
+            return new Response<>(authenticationInformationFromToken(codeInfo), Optional.empty(), 0, 1, "");
+        } catch (Exception ex) {
+            return new Response<>(null, Optional.of(new com.mrakaki.api.dtos.Error(500, ex.getMessage())), 0, 1, null);
+        }
+    }
+
+    private static AuthenticationInformation authenticationInformationFromToken(AuthenticationResponse codeInfo) throws Exception {
+
+        String[] chunks = codeInfo.access_token().split("\\.");
+
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String payload = new String(decoder.decode(chunks[1]));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readTree(payload);
+        int characterId = Integer.parseInt(node.get("sub").asText().split(":")[2]);
+        String characterName = node.get("name").asText();
+        return new AuthenticationInformation(characterId, characterName, codeInfo.access_token(), codeInfo.refresh_token());
+    }
+
 
     private static String basicAuthentication(String clientId, String clientSecret) {
         return "Basic " + Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes());
